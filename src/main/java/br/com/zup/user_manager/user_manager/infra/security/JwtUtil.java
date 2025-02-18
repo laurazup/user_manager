f82947a6f568a9474543ccf0b7b2f174df8833ef
laurazup <laura.rodrigues@zup.com.br>
2025-02-18 15:21:58 -0300

    chore: adiciona classe JwtUtil

---
+++ src/main/java/br/com/zup/user_manager/user_manager/infra/security/JwtUtil.java
@@ -0,0 +1,33 @@
+package br.com.zup.user_manager.user_manager.infra.security;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.Claims;
+
+public class JwtUtil {
+    private final String SECRET_KEY = "palavra_oculta_xablau";
+
+    private String createToken (Map<String, Object> claims, String subject) {
+        String token = Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 10 * 60 * 60 * 1000))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+        return token;
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJwt(token).getBody();
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+}
